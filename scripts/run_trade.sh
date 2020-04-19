39712400e79b54328c59aed3baba76b3aa5e4eb2
Adrien Barbot <catzhead@hotmail.com>
2020-04-19 10:59:35 +0200

    added script dir for convenience

---
+++ scripts/run_trade.sh
@@ -0,0 +1,5 @@
+CONFIG_STD="user_data/config/config.json"
+CONFIG_TELEGRAM="user_data/config/config-telegram-simu.json"
+CONFIG_EXCHANGE="user_data/config/config-exchange-binance-notrade.json"
+
+docker-compose run freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM -c $CONFIG_EXCHANGE --strategy BB3RSIStrategy
