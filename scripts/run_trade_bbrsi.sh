6a5ed7b03bb96d331719b28b81cbf680413cadb3
Adrien Barbot <catzhead@hotmail.com>
2020-04-26 11:20:15 +0200

    typo

--- scripts/run_trade_bbrsi.sh
+++ scripts/run_trade_bbrsi.sh
@@ -3,5 +3,5 @@ CONFIG_TELEGRAM="user_data/config/config-telegram-bbrsi.json"
 CONFIG_EXCHANGE="user_data/config/config-exchange-binance-notrade.json"
 
 docker-compose run freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM \
--c $CONFIG_EXCHANGE --strategy BB3RSIStrategy\
+-c $CONFIG_EXCHANGE --strategy BB3RSIStrategy \
 --logfile /freqtrade/user_data/log/bb3rsi.log
