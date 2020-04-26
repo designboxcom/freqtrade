f7a89c97888fd43c5e043f6412ebcbdc7bc2bce4
Adrien Barbot <catzhead@hotmail.com>
2020-04-26 11:21:48 +0200

    added daemon flag in run_trade scripts

--- scripts/run_trade_bbl3h2rsistd.sh
+++ scripts/run_trade_bbl3h2rsistd.sh
@@ -2,6 +2,6 @@ CONFIG_STD="user_data/config/config.json"
 CONFIG_TELEGRAM="user_data/config/config-telegram-bbl3h2rsistd.json"
 CONFIG_EXCHANGE="user_data/config/config-exchange-binance-notrade.json"
 
-docker-compose run freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM \
+docker-compose run -d freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM \
 -c $CONFIG_EXCHANGE --strategy BBL3H2RSIStdStrategy \
 --logfile /freqtrade/user_data/log/bbl3h2rsistd.log
--- scripts/run_trade_bbrsi.sh
+++ scripts/run_trade_bbrsi.sh
@@ -2,6 +2,6 @@ CONFIG_STD="user_data/config/config.json"
 CONFIG_TELEGRAM="user_data/config/config-telegram-bbrsi.json"
 CONFIG_EXCHANGE="user_data/config/config-exchange-binance-notrade.json"
 
-docker-compose run freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM \
+docker-compose run -d freqtrade trade -c $CONFIG_STD -c $CONFIG_TELEGRAM \
 -c $CONFIG_EXCHANGE --strategy BB3RSIStrategy \
 --logfile /freqtrade/user_data/log/bb3rsi.log
