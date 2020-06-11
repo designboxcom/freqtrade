2b089432ca58b1503a34846a753871e5f07aea67
Adrien Barbot <catzhead@hotmail.com>
2020-06-11 21:09:11 +0000

    latest hyperopt script

--- scripts/run_hyperopt.sh
+++ scripts/run_hyperopt.sh
@@ -3,7 +3,7 @@ CONFIG_STD="user_data/config/config.json"
 CONFIG_TELEGRAM="user_data/config/config-telegram-simu.json"
 CONFIG_EXCHANGE="user_data/config/config-exchange-binance-notrade.json"
 NBDAYS=90
-EPOCHS=20000
+EPOCHS=10000
 
 source .env/bin/activate
 python scripts/telegram-send.py --config user_data/config/config-telegram-simu.json "computation started" && docker-compose run freqtrade download-data -c $CONFIG_STD -c $CONFIG_TELEGRAM -c $CONFIG_EXCHANGE -t 1h --days $NBDAYS --exchange binance && docker-compose run -d freqtrade hyperopt -c $CONFIG_STD -c $CONFIG_TELEGRAM -c $CONFIG_EXCHANGE --strategy BB3RSIForHyperoptsStrategy --hyperopt HyperOptBBL3RSIH2 --logfile /freqtrade/user_data/log/hyperopt_bbrsi.log --hyperopt-loss SharpeHyperOptLoss -e $EPOCHS | xargs docker wait && python scripts/telegram-send.py --config user_data/config/config-telegram-simu.json "computation complete" &
